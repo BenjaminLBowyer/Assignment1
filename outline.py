2d4e9e0d6ec28e8c62199a63be058b8d9d4e0252
Benjamin <Benjaminlbowyer@gmail.com>
2026-06-08 18:27:13 -0500

    This is my first commit

---
+++ outline.py
@@ -0,0 +1,26 @@
+#T
+targetCells = 0
+
+#I
+infectedCells = 0
+
+#V
+virusParticles = 0
+
+#how fast is the number of target cells changing? ( dT/ dt )
+#how fast is the number of infected cells changing? ( dI/ dt )
+#how fast is the number of virus population changing? ( dV/ dt )
+
+#B / hour
+frequencyOfInfection = 1e-5
+
+#O / delta / hour
+deathRateOfInfectedCells = 4
+
+#p / hour
+productionRate = 2e6
+
+#c / hour
+clearanceRate = 4
+
+
